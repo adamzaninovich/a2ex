c614de0c203e18497ced14827f513980a9b0a7cc
Adam Zaninovich <adam.zaninovich@gmail.com>
2017-08-21 11:31:40 -0700

    generates robot

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,16 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :a2ex, A2ex.Robot,
+  adapter: Hedwig.Adapters.Console,
+  name: "rebecca",
+  aka: "/",
+  responders: [
+    {Hedwig.Responders.Help, []},
+    {Hedwig.Responders.Ping, []}
+  ]
+
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
