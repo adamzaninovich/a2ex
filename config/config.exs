1857089cc06b84c23c98dae8588eae4a35c332b5
Adam Zaninovich <adam.zaninovich@gmail.com>
2017-08-29 23:44:21 -0500

    adds heroku config

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,8 @@
 use Mix.Config
 
 config :a2ex, A2ex.Robot,
-  adapter: Alice.Adapters.Console,
+  adapter: Alice.Adapters.Slack,
+  slack_token: System.get_env("LAASY_SLACK_KEY"),
   name: "alice",
   log_level: :debug,
   handlers: [A2ex.Random]
